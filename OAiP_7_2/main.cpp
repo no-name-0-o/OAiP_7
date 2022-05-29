2951447a5e974733b2c30015f7927363f7dbc043
no_name_0_o <lsufranovich96@gmail.com>
2022-05-29 12:55:11 +0300

    Initial Commit

---
+++ OAiP_7_2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OAiP_7_2
+//
+//  Created by Alexey Sufranovich on 29.05.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
